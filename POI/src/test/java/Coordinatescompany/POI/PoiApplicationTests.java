c4eae77e0ff3f7f1118cae1594130e0faee6a553
Erick Ruas <erick_ruas@hotmail.com>
2021-05-19 12:28:59 -0300

    Commit inicial após a criação do Maven Project

---
+++ POI/src/test/java/Coordinatescompany/POI/PoiApplicationTests.java
@@ -0,0 +1,13 @@
+package Coordinatescompany.POI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
