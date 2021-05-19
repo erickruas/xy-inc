814f2a31140ba7b6e2465877754680edc17ab99d
Erick Ruas <erick_ruas@hotmail.com>
2021-05-19 15:00:46 -0300

    Implementação método de cadastro de um POI, por meio de mapeamento da controller e tratamento pela camada de serviço.

--- POI/src/main/java/Coordinatescompany/POI/controller/PoiController.java
+++ POI/src/main/java/Coordinatescompany/POI/controller/PoiController.java
@@ -1,10 +1,22 @@
 package Coordinatescompany.POI.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import Coordinatescompany.POI.entities.Poi;
+import Coordinatescompany.POI.services.PoiServiceImpl;
 
 @RestController
 @RequestMapping("/pois")
 public class PoiController {
+	
+	@Autowired
+	private PoiServiceImpl poiServiceImpl;
 
+	@PostMapping("/cadastrar")
+	public Poi novoPoi(Poi poi) {
+		return poiServiceImpl.salvar(poi);
+	}
 }
--- POI/src/main/java/Coordinatescompany/POI/services/PoiServiceImpl.java
+++ POI/src/main/java/Coordinatescompany/POI/services/PoiServiceImpl.java
@@ -1,15 +1,28 @@
 package Coordinatescompany.POI.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 
+import Coordinatescompany.POI.repository.PoiRepository;
 import Coordinatescompany.POI.entities.Poi;
 
 public class PoiServiceImpl implements PoiService{
 
+	@Autowired
+	private PoiRepository poiRepository;
+	
 	@Override
 	public Poi salvar(Poi poi) {
-		// TODO Auto-generated method stub
-		return null;
+
+		if (poi.getNome( ) == null || poi.getNome().isEmpty()) {
+			throw new IllegalArgumentException("Preencha o nome do POI.");
+		} else if (poi.getCoordenadaX() < 0) {
+			throw new IllegalArgumentException("A coordenadaX deve ser igual ou maior que 0.");
+		} else if (poi.getCoordenadaY() < 0) {
+			throw new IllegalArgumentException("A coordenadaY deve ser igual ou maior que 0.");
+		} else {
+			return poiRepository.save(poi);
+		}
 	}
 
 	@Override
