988a6e67168e3328adedfb4fa2dc141dd6f0f677
Erick Ruas <erick_ruas@hotmail.com>
2021-05-20 16:07:32 -0300

    Configuração do swagger para criação da documentação da API.

--- POI/src/main/java/Coordinatescompany/POI/config/SegurancaConfig.java
+++ POI/src/main/java/Coordinatescompany/POI/config/SegurancaConfig.java
@@ -3,6 +3,7 @@ package Coordinatescompany.POI.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -26,6 +27,16 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("user").password("123").roles("ADMIN");
 	}
+	
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                                   "/configuration/ui",
+                                   "/swagger-resources/**",
+                                   "/configuration/security",
+                                   "/swagger-ui.html",
+                                   "/webjars/**");
+    }
 	
 	@Bean
 	public PasswordEncoder PasswordEncoder() {
