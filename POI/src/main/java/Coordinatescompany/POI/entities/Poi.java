e301f25acd0ff7c3ed752e24069052972b34518c
Erick Ruas <erick_ruas@hotmail.com>
2021-05-20 10:56:02 -0300

    Criação do metodo equals e hashcode na entidade POI.

--- POI/src/main/java/Coordinatescompany/POI/entities/Poi.java
+++ POI/src/main/java/Coordinatescompany/POI/entities/Poi.java
@@ -60,4 +60,39 @@ public class Poi {
 	public void setCoordenadaY(int coordenadaY) {
 		this.coordenadaY = coordenadaY;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + coordenadaX;
+		result = prime * result + coordenadaY;
+		result = prime * result + id;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Poi other = (Poi) obj;
+		if (coordenadaX != other.coordenadaX)
+			return false;
+		if (coordenadaY != other.coordenadaY)
+			return false;
+		if (id != other.id)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
 }
